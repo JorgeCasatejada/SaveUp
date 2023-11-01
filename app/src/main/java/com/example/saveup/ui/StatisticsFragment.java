13baaa707cfb1cd3d523134ebfa0bb08d726d65b
Gonzalo Rodríguez <gonzalo.rodriguez@abamobile.com>
2023-11-01 23:25:54 +0100

    Modificación del fragmento de estadísticas

--- app/src/main/java/com/example/saveup/ui/StatisticsFragment.java
+++ app/src/main/java/com/example/saveup/ui/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.example.saveup.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.saveup.R;
 import com.example.saveup.model.Account;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +22,8 @@ public class StatisticsFragment extends Fragment {
     private static final String ACCOUNT = "Account";
     private Account account;
     private View root;
+
+    private FloatingActionButton shareFab;
 
     public static StatisticsFragment newInstance(Account account) {
         StatisticsFragment fragment = new StatisticsFragment();
@@ -38,6 +42,37 @@ public class StatisticsFragment extends Fragment {
     }
 
     private void initializeVariables() {
+        shareFab = root.findViewById(R.id.shareFab);
+        shareFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareStatistics();
+            }
+        });
+    }
+
+    public void shareStatistics(){
+        /* es necesario hacer un intent con la constate ACTION_SEND */
+        /*Llama a cualquier app que haga un envío*/
+        Intent itSend = new Intent(Intent.ACTION_SEND);
+        /* vamos a enviar texto plano */
+        itSend.setType("text/plain");
+        // itSend.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{para});
+        itSend.putExtra(Intent.EXTRA_SUBJECT, "mono");
+                //getString(R.string.menu_item_compartir) + ": " + pelicula.getTitulo());
+        itSend.putExtra(Intent.EXTRA_TEXT, "con diarrea");
+//                getString(R.string.titulo)
+//                +": "+pelicula.getTitulo()+"\n"+
+//                getString(R.string.contenido)
+//                +": "+pelicula.getArgumento());
+
+        /* iniciamos la actividad */
+                /* puede haber más de una aplicacion a la que hacer un ACTION_SEND,
+                   nos sale un ventana que nos permite elegir una.
+                   Si no lo pongo y no hay activity disponible, pueda dar un error */
+        Intent shareIntent=Intent.createChooser(itSend, null);
+
+        startActivity(shareIntent);
 
     }
 
@@ -45,7 +80,7 @@ public class StatisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        root = inflater.inflate(R.layout.fragment_groups, container, false);
+        root = inflater.inflate(R.layout.fragment_statistics, container, false);
 
         initializeVariables();
 
