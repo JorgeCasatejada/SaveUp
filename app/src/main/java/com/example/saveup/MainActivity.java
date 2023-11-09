87d2fcd983174acea94300c03c90335f9f10a1ce
Gonzalo Rodríguez <gonzalo.rodriguez@abamobile.com>
2023-11-10 00:10:22 +0100

    Adición de estadísticas de quesito y línea

--- app/src/main/java/com/example/saveup/MainActivity.java
+++ app/src/main/java/com/example/saveup/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.saveup.model.Account;
+import com.example.saveup.model.Category;
 import com.example.saveup.model.Transaction;
 import com.example.saveup.ui.GroupsFragment;
 import com.example.saveup.ui.MainScreenFragment;
@@ -17,6 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -67,19 +69,19 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Transaction> loadTransactions() {
         ArrayList<Transaction> transactionsList = new ArrayList<>();
-        transactionsList.add(new Transaction(false, "Gasto 1", 1.12345, "Una dola,"));
-        transactionsList.add(new Transaction(false, "Gasto 2", 2.05, "tela catola,"));
-        transactionsList.add(new Transaction(false, "Gasto 3", 3, "quila, quilete,"));
-        transactionsList.add(new Transaction(true, "Gasto 4", 4.0000, "estaba la reina"));
-        transactionsList.add(new Transaction(false, "Gasto 5", 5.99, "en su gabinete,"));
-        transactionsList.add(new Transaction(false, "Gasto 6", 6.995, "vino Gil,"));
-        transactionsList.add(new Transaction(false, "Gasto 7", 7, "apagó el candil,"));
-        transactionsList.add(new Transaction(false, "Gasto 8", 8, "candil candilón,"));
-        transactionsList.add(new Transaction(false, "Gasto 9", 1000.10, " las veinte que las veinte son."));
-        transactionsList.add(new Transaction(true, "Gasto 10", 1000.10, "cuenta las  que  veinte son."));
-        transactionsList.add(new Transaction(false, "Gasto 11", 1000.10, " las veinte que las veinte ."));
-        transactionsList.add(new Transaction(true, "Gasto 12", 1000.10, "cuenta las veinte que las  son."));
-        transactionsList.add(new Transaction(false, "Gasto 13", 1000.10, "veinte son."));
+        transactionsList.add(new Transaction(false, "Gasto 1", 1.12345, Category.REGALOS, new Date(2023 - 1900, 0, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 2", 2.05, Category.ALIMENTACION, new Date(2023 - 1900, 1, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 3", 3, Category.OCIO, new Date(2023 - 1900, 2, 1), ""));
+        transactionsList.add(new Transaction(true, "Gasto 4", 4.00, Category.TECNOLOGIA, new Date(2023 - 1900, 3, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 5", 5.99, Category.TRANSPORTE, new Date(2023 - 1900, 4, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 6", 6.995, Category.OCIO, new Date(2023 - 1900, 5, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 7", 7, Category.EDUCACION, new Date(2023 - 1900, 6, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 8", 8, Category.VIVIENDA, new Date(2023 - 1900, 7, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 9", 10.10, Category.TRANSPORTE, new Date(2023 - 1900, 8, 1), ""));
+        transactionsList.add(new Transaction(true, "Gasto 10", 10.10, Category.OCIO, new Date(2023 - 1900, 9, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 11", 10.10, Category.OCIO, new Date(2023 - 1900, 9, 1), ""));
+        transactionsList.add(new Transaction(true, "Gasto 12", 10.10, Category.VIVIENDA, new Date(2023 - 1900, 11, 1), ""));
+        transactionsList.add(new Transaction(false, "Gasto 13", 10.10, Category.VIVIENDA, new Date(2023 - 1900, 11, 1), ""));
         return transactionsList;
     }
 
--- app/src/main/java/com/example/saveup/ui/StatisticsFragment.java
+++ app/src/main/java/com/example/saveup/ui/StatisticsFragment.java
@@ -1,6 +1,7 @@
 package com.example.saveup.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +11,41 @@ import androidx.fragment.app.Fragment;
 
 import com.example.saveup.R;
 import com.example.saveup.model.Account;
+import com.example.saveup.model.Category;
 import com.example.saveup.model.Transaction;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +59,9 @@ public class StatisticsFragment extends Fragment {
     private View root;
 
     private FloatingActionButton shareFab;
+
+    private LineChart lineChart;
+    private PieChart pieChart;
 
     public static StatisticsFragment newInstance(Account account) {
         StatisticsFragment fragment = new StatisticsFragment();
@@ -55,6 +87,140 @@ public class StatisticsFragment extends Fragment {
                 shareStatistics();
             }
         });
+
+        createLineChart();
+        createPieChart();
+    }
+
+    private void createLineChart() {
+        String[] values = new String[] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiempre", "Octubre", "Noviembre", "Diciembre"};
+
+        Map<Integer, List<Transaction>> map = getGroupedTransactions(2023 - 1900);
+
+        lineChart = root.findViewById(R.id.lineChart);
+
+        lineChart.setDragEnabled(false);
+        lineChart.setScaleEnabled(false);
+
+        LimitLine zeroLine = new LimitLine(0f, "");
+        zeroLine.setLineWidth(2f);
+
+        Description description = new Description();
+        description.setText("Ingresos / Gastos");
+        description.setTextSize(15f);
+        description.setPosition(550f, 100f);
+        lineChart.setDescription(description);
+        lineChart.getAxisRight().setDrawLabels(false);
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setAxisLineWidth(2f);
+        xAxis.setLabelCount(11);
+        xAxis.setLabelRotationAngle(45f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(values));
+
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.addLimitLine(zeroLine);
+        yAxis.setDrawLimitLinesBehindData(true);
+
+        List<Entry> entries = new ArrayList<>();
+        float balance = 0;
+        float maxBalance = 0;
+        float minBalance = 0;
+        for (int month = 0; month < 12; month++) {
+            List<Transaction> transactions = map.getOrDefault(month, new ArrayList<>());
+            for (Transaction transaction : transactions) {
+                balance += transaction.getSignedValue();
+                System.out.println(transaction.getSignedValue());
+                if (balance < minBalance) minBalance = balance;
+                else if (balance > maxBalance) maxBalance = balance;
+            }
+            entries.add(new Entry(month, balance));
+        }
+
+        yAxis.setAxisMaximum(maxBalance + 10f);
+        yAxis.setAxisMinimum(minBalance - 10f);
+
+        LineDataSet dataset = new LineDataSet(entries, "Balance");
+        dataset.setColor(Color.CYAN);
+        dataset.setLineWidth(2f);
+
+        LineData lineData = new LineData(dataset);
+
+        lineChart.getAxisRight().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+
+        lineChart.setData(lineData);
+        lineChart.animateXY(1000, 1000);
+    }
+
+    private Map<Integer, List<Transaction>> getGroupedTransactions(int year) {
+        account.getTransactionsList().forEach(System.out::println);
+        return account.getTransactionsList().stream().filter(t -> t.getDate().getYear() == year).collect(Collectors.groupingBy(t -> t.getDate().getMonth()));
+    }
+
+    private void createPieChart() {
+        pieChart = root.findViewById(R.id.pieChart);
+        ArrayList<PieEntry> categories = new ArrayList<>();
+
+        Map<Category, Double> map = getCategories();
+        map.keySet().forEach(category -> categories.add(
+                new PieEntry(Float.parseFloat(Objects.requireNonNull(map.get(category)).toString()),
+                        category.toString())));
+
+        int[] colors = {
+                Color.parseColor("#488f31"),
+//                Color.parseColor("#6a9832"),
+                Color.parseColor("#88a037"),
+//                Color.parseColor("#a5a73f"),
+                Color.parseColor("#c0af4a"),
+//                Color.parseColor("#dbb659"),
+                Color.parseColor("#f4bd6a"),
+//                Color.parseColor("#f2a95e"),
+                Color.parseColor("#ef9556"),
+//                Color.parseColor("#eb8050"),
+                Color.parseColor("#e56b4e"),
+//                Color.parseColor("#dd554f"),
+                Color.parseColor("#de425b"),
+        };
+
+        PieDataSet pieDataSet = new PieDataSet(categories, "Categorias");
+        pieDataSet.setColors(colors);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(10f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setForm(Legend.LegendForm.CIRCLE);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(5f);
+        l.setYEntrySpace(5f);
+        l.setYOffset(100f);
+        l.setWordWrapEnabled(false);
+
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Categorias");
+        pieChart.setCenterTextSize(15f);
+        pieChart.setEntryLabelTextSize(10f);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setHoleRadius(40f);
+        pieChart.setTransparentCircleRadius(45f);
+        pieChart.animateXY(1000, 1000);
+    }
+
+    private Map<Category, Double> getCategories() {
+        return account.getTransactionsList().stream().collect(
+                Collectors.groupingBy(Transaction::getCategory,
+                    Collectors.mapping(Transaction::getValue, Collectors.summingDouble(Double::doubleValue))));
     }
 
     public void shareStatistics(){
@@ -110,5 +276,18 @@ public class StatisticsFragment extends Fragment {
         initializeVariables();
 
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        System.out.println("///////////////////////////////////////////" +
+                "///////////////////////////////////////////" +
+                "///////////////////////////////////////////" +
+                "///////////////////////////////////////////" +
+                "///////////////////////////////////////////" +
+                "///////////////////////////////////////////");
+        createLineChart();
+        createPieChart();
     }
 }
